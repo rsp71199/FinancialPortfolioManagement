f10dc806e33fbac8ea3b252291514cdb3580627a
rutujaD1999 <rutuja138@gmail.com>
2020-08-24 15:51:07 +0530

    commit beofre pull

--- app/src/main/java/com/example/financialportfoliomanagement/Activities/ChartsActivity.java
+++ app/src/main/java/com/example/financialportfoliomanagement/Activities/ChartsActivity.java
@@ -5,12 +5,28 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.financialportfoliomanagement.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
-public class ChartsActivity extends AppCompatActivity {
+public class ChartsActivity extends AppCompatActivity  {
 
+    private static final String TAG = "ChartsActivity";
+
+    private LineChart mChart;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charts);
+
+        mChart = (LineChart) findViewById(R.id.linechart);
+//        mChart.setOnChartGestureListener(ChartsActivity.this);
+//        mChart.setOnChartValueSelectedListener(ChartsActivity.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(false);
+
+
     }
 }
